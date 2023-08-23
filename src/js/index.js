d8d51f1aedbdcb567a55215226f4c10f0a42cd70
Airowl <airowl.gasga@gmail.com>
2023-08-23 17:22:51 +0200

    fix requests

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,61 @@
-import '../scss/style.scss';
-import axios from 'axios';
+import "../scss/style.scss";
+import axios from "axios";
 
-var _ = require('lodash');
+var _ = require("lodash");
 
-console.log(_);
+const allNewsApi = process.env.ALL_NEWS_API;
+const singleNewsApi = process.env.SINGLE_NEWS_API;
 
-console.log('foo'); 
+let tBody = document.querySelector("tbody");
+let times = 1;
+let count = 10;
+const result = [];
+getNews();
+
+const loadMore = document.querySelector(".load-more");
+
+loadMore.addEventListener("click", (e) => {
+    e.preventDefault();
+    times += 1;
+    getNews();
+});
+
+function getNews() {
+    axios
+        .get(allNewsApi)
+        .then(function (response) {
+            const res = response.data;
+            for (let i = 0; i < res.length; i++) {
+                if (i <= (count * times) - 1 && i >= ((count * times) - count)) {
+                    getSingleNews(res[i]);
+                }
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+function getSingleNews(id) {
+    axios
+        .get(`${singleNewsApi}/${id}.json`)
+        .then(function (response) {
+            tBody.innerHTML += elementDom(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function elementDom(data) {
+    const { id, title, type, by, time, url } = data;
+    return `
+        <tr>
+            <th scope="row">${id}</th>
+            <td>${title}</td>
+            <td>${type}</td>
+            <td>${by}</td>
+            <td>${time}</td>
+            <td>${url}</td>
+        </tr>
+    `;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,43 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: "./src/js/index.js",
     output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
     devServer: {
-        static: path.resolve(__dirname, 'dist'),
+        static: path.resolve(__dirname, "dist"),
         open: true,
         port: 8085,
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'App webpack',
-            template: './src/index.html'
+            title: "App webpack",
+            template: "./src/index.html",
         }),
-        new Dotenv()
+        new Dotenv(),
     ],
     module: {
         rules: [
             {
                 test: /\.(css|s[ac]ss)$/i,
-                use: [
-                    'style-loader', 'css-loader', 'sass-loader'
-                ]
+                use: ["style-loader", "css-loader", "sass-loader"],
             },
-            {
-                test: /\.js$/i,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options:{
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    }
+            //{
+            //    test: /\.js$/i,
+            //    exclude: /node_modules/,
+            //    use: {
+            //        loader: 'babel-loader',
+            //        options:{
+            //            presets: ['@babel/preset-env']
+            //        }
+            //    }
+            //}
+        ],
+    },
 };
